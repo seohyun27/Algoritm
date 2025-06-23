3514b75a1f388ef3f36ee084956338e3c528cc99
Seohyun Kim <canna7610@gmail.com>
2025-06-23 15:08:37 +0900

    update code
    SH

--- Homework/homework4/1.java
+++ Homework/homework4/1.java
@@ -10,9 +10,9 @@ class Solution {
             System.arraycopy(triangle[i], 0, B[i], 0, triangle[i].length);
         }
 
-        for (int i = size - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                B[i][j] = B[i][j] + Math.max(B[i+1][j], B[i+1][j+1]);
+        for (int i = size - 2; i >= 0; i--) { //삼각형의 높이
+            for (int j = 0; j <= i; j++) { //해당 높이의 값들
+                B[i][j] = B[i][j] + Math.max(B[i+1][j], B[i+1][j+1]); //바로 아래 두 값 중에 높은 값을 선택해서 현재값에 더함
             }
         }
 
